0ec96b573fa669e847cfcef91e4185bafc49b79b
Inna Gosteva <innagosteva31@yandex.ru>
2023-05-04 21:44:08 +0500

    add padding/img

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 import card_1 from './img/portfolio_card1.jpg'
 import card_2 from './img/portfolio_card2.jpg'
 import card_3 from './img/portfolio_card3.jpg'
+import card_4 from './img/portfolio_card4.jpg'
 
 export const array = [
   {
@@ -20,29 +21,23 @@ export const array = [
   {
     id:3,
     img: card_3,
-    descriptionRU: "Маленький инстаграм",
-    descriptionEN: "Little instagram",
+    descriptionRU: "Инстаграм JavaScript",
+    descriptionEN: "Instagram JavaScript",
     link: 'https://github.com/innabunny/mesto',
   },
   {
     id: 4,
-    img: card_1,
-    descriptionRU: "Лендинг",
-    descriptionEN: "Landing page",
-    link: 'https://github.com/innabunny/Sprint-2-How-to-learn',
+    img: card_3,
+    descriptionRU: "Инстаграм React",
+    descriptionEN: "Instagram React",
+    link: 'https://github.com/innabunny/react-mesto-api-full',
   },
   {
     id: 5,
-    img: card_2,
-    descriptionRU: "Адаптивная верстка",
-    descriptionEN: "Adaptive layout",
-    link: 'https://github.com/innabunny/russian-travel',
+    img: card_4,
+    descriptionRU: "Сервис поиска фильмов",
+    descriptionEN: "Movies explorer",
+    link: 'https://github.com/innabunny/movies-explorer-frontend',
   },
-  {
-    id: 6,
-    img: card_3,
-    descriptionRU: "Маленький инстаграм",
-    descriptionEN: "Little instagram",
-    link: 'https://github.com/innabunny/mesto',
-  }
+  
 ]
