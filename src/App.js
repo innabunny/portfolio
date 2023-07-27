a26738de4cdf2cf97346cbc783b1ebe6b29b60e1
Inna Gosteva <innagosteva31@yandex.ru>
2023-07-27 21:17:34 +0300

    add icons header

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/header/Header';
-import About from './components/About/About';
-import Content from './components/Content/Content';
-import Footer from './components/Footer/Footer';
-import { useState } from 'react';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+
+import Header from "./components/header/Header";
+import About from "./components/About/About";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+import Resume from "./components/Resume/Resume";
 
 function App() {
-
   const [isTranslate, setIsTranslate] = useState(false);
-  
-    const translateRU = () => {
-      setIsTranslate(false);
-    }
-  
-    const translateEN = () => {
-      setIsTranslate(true);
-    }
-  
+
+  const translateRU = () => {
+    setIsTranslate(false);
+  };
+
+  const translateEN = () => {
+    setIsTranslate(true);
+  };
+
   return (
-   <>
-    <Header isTranslate={isTranslate} translateEN={translateEN} translateRU={translateRU} />
-    <About isTranslate={isTranslate} />
-    <Content isTranslate={isTranslate}/>
-    <Footer isTranslate={isTranslate} />
-   </>
+    <BrowserRouter>
+      <Header
+        isTranslate={isTranslate}
+        translateEN={translateEN}
+        translateRU={translateRU}
+      />
+      <Routes>
+        <Route path="/resume" element={Resume} />
+      </Routes>
+      <About isTranslate={isTranslate} />
+      <Content isTranslate={isTranslate} />
+      <Footer isTranslate={isTranslate} />
+    </BrowserRouter>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,32 @@
-import "./Header.css"
-import icon from "../../img/github.png"
+import { Link, NavLink } from 'react-router-dom';
+import Nav from "react-bootstrap/Nav";
+import "./Header.css";
+import icon from "../../img/github.png";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+} from "react-icons/ai";
+
+import { CgFileDocument } from "react-icons/cg";
+import { FaRegCalendarCheck } from "react-icons/fa";
 
 export default function Header({translateRU, translateEN, isTranslate}) {
+  
   return (
-    <header className="header">
-      <a href="https://github.com/innabunny" target="_blank" className="header__link"><img src={icon} className="header__img"></img></a>
-      <nav>
+<>
+     <Nav className='header'>
+      <Nav.Link href="https://github.com/innabunny" target="_blank" className="header__link"><img src={icon} className="header__img"></img></Nav.Link>
         <ul className="header__links">
-          <li><a href="#project" className="header__link">{ isTranslate ? "Home" : "Главная"}</a></li>
-          <li><a href="#project" className="header__link">{ isTranslate ? "About Me" : "Обо мне"}</a></li>
-          <li><a href="#contacts" className="header__link">{ isTranslate ? "Resume" : "Резюме"}</a></li>
-          <li><a href="#contacts" className="header__link">{ isTranslate ? "Projects" : "Проекты"}</a></li>
+          <li><Link to='/home' className="header__link"><AiOutlineHome /> {isTranslate ? 'Home' : 'Главная'}</Link></li>
+          <li><Link to='/about' className="header__link"><AiOutlineUser /> {isTranslate ? 'About' : 'Обо мне'}</Link></li>
+          <li><Link to='/resume' href="#contacts" className="header__link"><CgFileDocument /> {isTranslate ? 'Resume' : 'Резюме'}</Link></li>
+          <li><Link to='/project' href="#contacts" className="header__link"><FaRegCalendarCheck /> {isTranslate ? 'Projects' : 'Проекты'}</Link></li>
         </ul>
-      </nav>
       <div>
         <button className="header__btn" onClick={translateRU}>Ru</button>
         <button className="header__btn" onClick={translateEN}>En</button>
       </div>
-    </header>
+      </Nav>
+</>
   );
 }
